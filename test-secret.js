30d734af4bf82851a0a08100a6e698b68dc435ff
AmnihYousef <amnih.yousef1@gmail.com>
2025-09-17 21:29:49 +0300

    Create test-secret.js

---
+++ test-secret.js
@@ -0,0 +1,4 @@
+// test-secret.js
+const AWS_KEY = "AKIAABCDEFGHIJKLMNOP"; // fake AWS key for test
+const slack = "https://hooks.slack.com/services/T000/B000/XXXXX"; // fake slack webhook
+const password = "mypassword123"; // hardcoded password (for test)
