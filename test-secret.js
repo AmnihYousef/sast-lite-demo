c495b8f9867d8a611204d69402becb2f0f9d1a25
AmnihYousef <amnih.yousef1@gmail.com>
2025-09-18 10:29:25 +0300

    Update test-secret.js

--- test-secret.js
+++ test-secret.js
@@ -4,3 +4,4 @@ const slack = "https://hooks.slack.com/services/T000/B000/XXXXX"; // fake slack 
 const password = "mypassword123"; // hardcoded password (for test)
 
 // new test line
+// test change for webhook
