c93ec3692cfe91ba16007a9be79b5f2e4ee135d9
AmnihYousef <amnih.yousef1@gmail.com>
2025-09-18 10:26:46 +0300

    Update test-secret.js

--- test-secret.js
+++ test-secret.js
@@ -3,5 +3,4 @@ const AWS_KEY = "AKIAABCDEFGHIJKLMNOP"; // fake AWS key for test
 const slack = "https://hooks.slack.com/services/T000/B000/XXXXX"; // fake slack webhook
 const password = "mypassword123"; // hardcoded password (for test)
 
-// new secret added for testing
 // new test line
